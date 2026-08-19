fccbe61bf08e86f5eaa4ada771a62928136a5540
Getter <459319819@qq.com>
2026-08-19 16:43:31 +0800

    优化main函数

--- src/main/java/com/wonton/Main.java
+++ src/main/java/com/wonton/Main.java
@@ -1,7 +1,21 @@
 package com.wonton;
 
+import com.wonton.logger.Logger;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Logger.success("Hello Wonton:");
+        Logger.debug("args:" + Arrays.toString(args));
+        if (args.length > 1) {
+            Logger.primary("用法: wonton [script]");
+            // 退出码64：命令使用错误，立即终止程序
+            System.exit(64);
+        } else if (args.length == 1) {
+            // runFile(args[0]);
+        } else {
+            // runPrompt();
+        }
     }
 }
