7dd0f839e1e8e42a6c4afdb9e4e4a73b8761ec48
Getter <459319819@qq.com>
2026-08-23 02:38:19 +0800

    test: 新增关系运算符单元测试

--- src/test/java/com/wonton/InterpreterTest.java
+++ src/test/java/com/wonton/InterpreterTest.java
@@ -56,4 +56,28 @@ public class InterpreterTest {
         RuntimeValue result = interpreter.interpret(ast);
         Logger.success("结果: {0}", result);
     }
+
+    @Test
+    public void testComparisonExpression() {
+        // 源代码
+        String source = FileUtils.readSource("scripts/comparison_expression.wonton");
+        Logger.debug("源代码: {0}", source);
+
+        // tokens
+        Lexer lexer = new Lexer(source);
+        List<Token> tokens = lexer.tokenize();
+        for (var i = 0; i < tokens.size(); i++) {
+            Logger.debug("序号: {0} {1}", String.format("%-6s", i+1), tokens.get(i));
+        }
+
+        // 语法树
+        Parser parser = new Parser(tokens);
+        Node ast = parser.parse();
+        Logger.debug("\n" + ast.toPrettyString());
+
+        // 解释执行
+        Interpreter interpreter = new Interpreter();
+        RuntimeValue result = interpreter.interpret(ast);
+        Logger.success("结果: {0}", result);
+    }
 }
